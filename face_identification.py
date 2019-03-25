260d6ed725b1bc13be0b6c171dac7e6c8b8c7b5a
jack <=>
2019-03-25 22:31:18 +0530

    Convert to OOP

--- face_identification.py
+++ face_identification.py
@@ -2,170 +2,168 @@ import cv2
 import numpy as np
 import os
 
-#corresponding person
+#subjects
 subjects = ["Will","Mark"]
 
+#face_identification class
+class FaceIdentification:
+    
+    def __init__(self,subjects):
+        self.subjects = subjects
 
-#function to detect face using OpenCV
-def detect_face(img):
-    #convert the test image to gray scale
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    
-    #load OpenCV face detector
-    face_cascade = cv2.CascadeClassifier('cascade/lbpcascade_frontalface.xml')
-    
-    #detect multiscale images
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5);
-    
-    #if no faces are detected then return None
-    if (len(faces) == 0):
-        return None, None
-    
-    #extract the face area assuming there is only one face
-    x, y, w, h = faces[0]
-    
-    #return only the face part of the image
-    return gray[y:y+w, x:x+h], faces[0]
+    #function to detect face using OpenCV
+    def detect_face(self,img):
+        #convert the test image to gray scale
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        
+        #load OpenCV face detector
+        face_cascade = cv2.CascadeClassifier('cascade/lbpcascade_frontalface.xml')
+        
+        #detect multiscale images
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5);
+        
+        #if no faces are detected then return None
+        if (len(faces) == 0):
+            return None, None
+        
+        #extract the face area assuming there is only one face
+        x, y, w, h = faces[0]
+        
+        #return only the face part of the image
+        return gray[y:y+w, x:x+h], faces[0]
 
-#this function will read all persons' training images, detect face from each image
-#and will return two lists of exactly same size, one list 
-#of faces and another list of labels for each face
-def prepare_training_data(data_folder_path):
-    #------STEP-1--------#
-    #get the directories (one directory for each subject) in data folder
-    dirs = os.listdir(data_folder_path)
-    
-    #list to hold all subject faces
-    faces = []
-    #list to hold labels for all subjects
-    labels = []
-    
-    #let's go through each directory and read images within it
-    for dir_name in dirs:
-    
-        #our subject directories start with letter 's' so
-        #ignore any non-relevant directories if any
-        if not dir_name.startswith("p"):
-            continue;
+    #this function will read all persons' training images, detect face from each image
+    #and will return two lists of exactly same size, one list 
+    #of faces and another list of labels for each face
+    def prepare_training_data(self,data_folder_path):
         
-        #------STEP-2--------
-        #extract label number of subject from dir_name
-        #format of dir name = slabel
-        #, so removing letter 's' from dir_name will give us label
-        label = int(dir_name.replace("p", ""))
+        #get the directories 
+        dirs = os.listdir(data_folder_path)
         
-        #build path of directory containing images for current subject subject
-        #sample subject_dir_path = "training-data/s1"
-        subject_dir_path = data_folder_path + "/" + dir_name
+        #list to hold all subject faces
+        faces = []
+        #list to hold labels for all subjects
+        labels = []
         
-        #get the images names that are inside the given subject directory
-        subject_images_names = os.listdir(subject_dir_path)
-
-
-        #------STEP-3--------
-        #go through each image name, read image, 
-        #detect face and add face to list of faces
-        for image_name in subject_images_names:
+        #get each image in dir
+        for dir_name in dirs:
         
-            #ignore system files like .DS_Store
-            if image_name.startswith("."):
+            #ignore any non-relevant directories if any
+            if not dir_name.startswith("p"):
                 continue;
             
-            #build image path
-            #sample image path = training-data/s1/1.pgm
-            image_path = subject_dir_path + "/" + image_name
+            #get label
+            label = int(dir_name.replace("p", ""))
+            
+            #build path of directory containing images for current subject subject
+            subject_dir_path = data_folder_path + "/" + dir_name
+            
+            #get the images names that are inside the given subject directory
+            subject_images_names = os.listdir(subject_dir_path)
 
-            #read image
-            image = cv2.imread(image_path)
+
+            #go through each image name, read image, 
+            #detect face and add face to list of faces
+            for image_name in subject_images_names:
             
-            #display an image window to show the image 
-            cv2.imshow("Training on image...", image)
-            #cv2.waitKey(100)
+                #ignore system files like .DS_Store
+                if image_name.startswith("."):
+                    continue;
+                
+                #build image path
+                #sample image path = training-data/s1/1.pgm
+                image_path = subject_dir_path + "/" + image_name
+
+                #read image
+                image = cv2.imread(image_path)
+                
+                #display an image window to show the image 
+                cv2.imshow("Training on image...", image)
+                cv2.waitKey(100)
+                
+                #detect face
+                face, rect = self.detect_face(image)
+                
+                
+                #ignore faces that are not detected
+                if face is not None:
+                    #add face to list of faces
+                    faces.append(face)
+                    #add label for this face
+                    labels.append(label)
+                    print(image_name+" read sucessfully!!")
+                else:
+                    print(image_name+" can't detect face!")
             
-            #detect face
-            face, rect = detect_face(image)
+        cv2.destroyAllWindows()
+        cv2.waitKey(1)
+        cv2.destroyAllWindows()
             
-            #------STEP-4--------
-            #for the purpose of this tutorial
-            #we will ignore faces that are not detected
-            if face is not None:
-                #add face to list of faces
-                faces.append(face)
-                #add label for this face
-                labels.append(label)
-                print(image_name+" read sucessfully!!")
-            else:
-                print(image_name+" can't detect face!")
+        return faces, labels
+
+    def fetch_data(self):
+        #fetch data
+        print("Preparing data...")
+        self.faces, self.labels = self.prepare_training_data("training_data")
+        print("Data prepared")
+
+        #print total faces and labels
+        print("Total faces: ", len(self.faces))
+        print("Total labels: ", len(self.labels))
+
+    def trainData(self):
+        #face recogizing algorithm 
+        self.face_recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+        #training algorithm
+        self.face_recognizer.train(self.faces, np.array(self.labels))
+
+    #function to draw rectangle on image 
+    def draw_rectangle(self,img, rect):
+        (self.x, self.y, self.w, self.h) = rect
+        cv2.rectangle(img, (self.x, self.y), (self.x+self.w, self.y+self.h), (0, 255, 0), 2)
+    
+    #function to draw text on give image from x,y 
+    def draw_text(self,img, text, x, y):
+        cv2.putText(img, text, (x, y), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 255, 0), 2)
+
+
+    #predict the image of the given person
+    def predict(self,test_img):
+
+        img = test_img.copy()
+
+        #detect face from the image
+        face, rect = self.detect_face(img)
+
+        #predict the image using our face recognizer 
+        label = self.face_recognizer.predict(face)
         
-    cv2.destroyAllWindows()
-    cv2.waitKey(1)
-    cv2.destroyAllWindows()
+        #get name of respective label returned by face recognizer
+        label_text = self.subjects[label[0]]
         
-    return faces, labels
+        print("Accuracy:"+str(label[1]))
+
+        #draw a rectangle around face detected
+        self.draw_rectangle(img, rect)
+        #draw name of predicted person
+        self.draw_text(img, label_text, rect[0], rect[1]-5)
+        
+        return img
 
 
-#let's first prepare our training data
-#data will be in two lists of same size
-#one list will contain all the faces
-#and the other list will contain respective labels for each face
-print("Preparing data...")
-faces, labels = prepare_training_data("training_data")
-print("Data prepared")
+fi = FaceIdentification(subjects)
+fi.fetch_data()
+fi.trainData()
 
-#print total faces and labels
-print("Total faces: ", len(faces))
-print("Total labels: ", len(labels))
-
-
-#create our LBPH face recognizer 
-face_recognizer = cv2.face.LBPHFaceRecognizer_create()
-
-#train our face recognizer of our training faces
-face_recognizer.train(faces, np.array(labels))
-
-#function to draw rectangle on image 
-#according to given (x, y) coordinates and 
-#given width and heigh
-def draw_rectangle(img, rect):
-    (x, y, w, h) = rect
-    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
- 
-#function to draw text on give image starting from
-#passed (x, y) coordinates. 
-def draw_text(img, text, x, y):
-    cv2.putText(img, text, (x, y), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 255, 0), 2)
-
-#this function recognizes the person in image passed
-#and draws a rectangle around detected face with name of the 
-#subject
-def predict(test_img):
-    #make a copy of the image as we don't want to change original image
-    img = test_img.copy()
-    #detect face from the image
-    face, rect = detect_face(img)
-
-    #predict the image using our face recognizer 
-    label = face_recognizer.predict(face)
-    
-    #get name of respective label returned by face recognizer
-    label_text = subjects[label[0]]
-    
-    print("Accuracy:"+str(label[1]))
-
-    #draw a rectangle around face detected
-    draw_rectangle(img, rect)
-    #draw name of predicted person
-    draw_text(img, label_text, rect[0], rect[1]-5)
-    
-    return img
 
 print("Predicting images...")
 
 #load test images
-test_img = cv2.imread("sample_data/3.jpg")
+test_img = cv2.imread("test_data/3.jpg")
 
 #perform a prediction
-predicted_img = predict(test_img)
+predicted_img = fi.predict(test_img)
 print("Prediction complete")
 
 #display both images
